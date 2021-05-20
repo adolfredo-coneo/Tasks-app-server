be2cd2ffc9b0b04724e1b0aa083544fb56dd8d0b
Adolfredo Coneo <ado.coneo@gmail.com>
2021-05-20 00:08:59 -0500

    Axios Request

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 const express = require("express");
+const middleware = require("./middlewares/middlewares.js");
+const routes = require("./routes");  
 
 const app = express();
 const port = 8080;
 
-app.get("/tasks/:N", (request, response) => {
-    response
-    .status(200)
-    .send(
-        `List of Tasks ${request.params.N}`
-    );
-});
-
-app.put("/tasks/:id", (request, response) => {
-  //response.send("This flow is triggered when a Get request is made on the /test path");
-  response
-    .status(200)
-    .send(
-        `Task ${request.params.id} mark as completed`
-    );
-});
+app.use(middleware.multipleMiddleware);
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`We are running at http://localhost:${port}`);
